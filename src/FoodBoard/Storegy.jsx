4e74141db2fed4d7547d7edc9d77e9ad857d6179
wooB0328 <145775269+wooB0328@users.noreply.github.com>
2023-11-20 12:37:17 +0900

    Update Storegy.jsx

--- src/FoodBoard/Storegy.jsx
+++ src/FoodBoard/Storegy.jsx
@@ -1,6 +1,4 @@
-import React, { useReducer } from 'react';
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, onValue } from "firebase/database";
+import { initializeApp, getApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCdGCsFCvfwgJg23ToUNl8KpEiGM41fotY",
@@ -12,47 +10,13 @@ const firebaseConfig = {
   appId: "1:216874707741:web:ebcb269baea2880e698ff4"
 };
 
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-
-// Define the initial state for the database
-const initialState = database;
-
-// Define the reducer function
-function databaseReducer(state, action) {
-  switch (action.type) {
-    case 'REGISTER_DATABASE':
-      return {
-        ...state,
-        items: [...state.items, action.payload],
-      };
-    case 'WRITE_ITEM':
-      return {
-        ...state,
-        items: state.items.filter((item) => item !== action.payload),
-      };
-    default:
-      return state;
-  }
+// Check if Firebase app is already initialized
+let app;
+try {
+  app = getApp();
+} catch (e) {
+  app = initializeApp(firebaseConfig);
 }
 
-// Create a context to provide the database and dispatch functions
-const DatabaseContext = React.createContext(initialState);
 
-// Custom hook to use the database and dispatch functions
-function useDatabase() {
-  return React.useContext(DatabaseContext);
-}
-
-// Database provider component
-function DatabaseProvider({ children }) {
-  const [database, dispatch] = useReducer(databaseReducer, initialState);
-
-  return (
-    <DatabaseContext.Provider value={{ database, dispatch }}>
-      {children}
-    </DatabaseContext.Provider>
-  );
-}
-
-export { useDatabase, DatabaseProvider };
+export { app };
